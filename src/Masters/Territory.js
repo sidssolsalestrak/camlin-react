335e7e9e8141cb30212e76be8a40583773f1710a
amal <amal@sidssol.com>
2026-03-25 15:16:27 +0530

    ter changes

--- src/Masters/Territory.js
+++ src/Masters/Territory.js
@@ -4,13 +4,16 @@ import { TextField, Box, Typography, Button } from "@mui/material";
 import api from "../services/api";
 import { useSnackbar } from "notistack";
 import PageHeader from "../utils/PageHeader";
+import { useNavigate,useParams } from "react-router-dom";
 
 export default function Territory() {
+       const { editTeritoryId }=useParams()
+       const navigate=useNavigate()
+    
        return(
         <Layout>
         <PageHeader title="Territory" />
         <Box sx={{ backgroundColor: 'white', mt: 3, ml: 2, borderRadius: '6px', minHeight: '30vh', width: {lg:'60%',md:'80%',sm:'90%',xs:'90%'} }}>
-        
         </Box>
         
         </Layout>
