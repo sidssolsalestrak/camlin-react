1462bd4469b8a796827c1a252e530cbd56a498c4
apple <apple@apples-MacBook-Pro.local>
2026-06-04 14:16:55 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -62,6 +62,7 @@ import PrimarySales from "./DashBoardReports/TrendAnalysis/PrimarySales";
 import PrimarySalesAnalze from "./DashBoardReports/PrimaryAnalysis/primarySalesAnalyse";
 import SalesAnalysisReport from "./DashBoardReports/PrimaryAnalysis/SalesAnalysisReport";
 import AreaWiseSalesAnalysis from "./dashboard/AreaWiseSalesAnalysis";
+import StockAndSalesUpload from "./view/trans/StockAndSalesUpload";
 
 function App() {
   const ProtectedRoute = () => {
@@ -286,6 +287,8 @@ function App() {
           <Route path="/reports/performance_report" element={<SalesAnalysis />} />
           <Route path="/dashboard/districtsales" element={<AreaWiseSalesAnalysis />} />
 
+          {/* TRANSACTIONS */}
+          <Route path="/input/stock_sales" element={<StockAndSalesUpload />} />
         </Routes>
       </BrowserRouter>
       {/* </SnackbarProvider> */}
