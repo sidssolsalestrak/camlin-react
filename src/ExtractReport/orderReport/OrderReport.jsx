d964ad08a80f4ea79085a3663513bc4e232f5293
amal <amal@sidssol.com>
2026-08-05 10:29:45 +0530

    area stk updt

--- src/ExtractReport/orderReport/OrderReport.jsx
+++ src/ExtractReport/orderReport/OrderReport.jsx
@@ -139,9 +139,9 @@ const OrderReport = () => {
         try {
             let payload = {
                 reg_id: formData.region,
-                zone_id: null
+                zone_id:formData.zone
             }
-            const res = await axios.post("/extractAreaList", payload);
+            const res = await axios.post("/AreaAm", payload);
             const data = Array.isArray(res?.data?.data) ? res?.data?.data : []
             setarea(data);
         } catch (error) {
--- src/Masters/main/stockist/Stockist.jsx
+++ src/Masters/main/stockist/Stockist.jsx
@@ -270,7 +270,7 @@ const Stockist = () => {
                 reg_id: region,
                 zone_id: null
             }
-            const res = await axios.post("/extractAreaList", payload);
+            const res = await axios.post("/AreaAm", payload);
             const data = Array.isArray(res?.data?.data) ? res?.data?.data : []
             setarea(data);
         } catch (error) {
