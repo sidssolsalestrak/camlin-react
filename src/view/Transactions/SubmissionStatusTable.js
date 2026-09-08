4d050803375ba35ee59bed22d77a80132cfc7111
amal <amal@sidssol.com>
2026-09-08 23:53:57 +0530

    submission chngs

--- src/view/Transactions/SubmissionStatusTable.js
+++ src/view/Transactions/SubmissionStatusTable.js
@@ -445,7 +445,7 @@ const SubmissionStatusTableComponent = ({
                     {renderStatusCell(row)}
                     {Number(row.base_data_stat) === 1 && row.process_stat !== 2 && row.process_stat !== 3 && (
                         <Tooltip title="Reject">
-                            <span style={{ cursor: "pointer", display: "flex", alignItems: "center", justifyContent: "center", width: "10px", height: "10px", border:"0.1px solid red", borderRadius: "3px" }} onClick={() => onReject?.(row.primary_id)}>
+                            <span style={{ cursor: "pointer", display: "flex", alignItems: "center", justifyContent: "center", width: "10px", height: "10px", border:"0.1px solid red", borderRadius: "3px",marginTop:'10px' }} onClick={() => onReject?.(row.primary_id)}>
                                 <FaMinus style={{ color: "red", fontSize: 6 }} />
                             </span>
                         </Tooltip>
@@ -453,6 +453,11 @@ const SubmissionStatusTableComponent = ({
                 </Box>
             );
         }
+
+        if (field === "err_desc") {
+        return <div style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", width: "100%", display: "block" }}>{formatCellData(row[field], field)}</div>;
+        }
+ 
 
         if (field === "rate_score" && stkGroup === 1) {
             if (row._rowType === "data") return renderStarRating(row.rate_score, row.process_stat);
