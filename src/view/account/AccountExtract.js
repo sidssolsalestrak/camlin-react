32812f3cd1d7e15d5d2b68db77c687dce46751fc
amal <amal@sidssol.com>
2026-09-16 22:35:21 +0530

    acc extr chngs

--- src/view/account/AccountExtract.js
+++ src/view/account/AccountExtract.js
@@ -183,7 +183,7 @@ function AccountExtract() {
 
       const meta = {
         "": `${masterPanel["ACCM"] || "Account"} Master Extract (${AccName})`,
-        "Date": dayjs().format("DD MMM YYYY")
+        Date: `Date:${dayjs().format("DD MMM YYYY")}`
       }
       DownloadCSV(
         FormattedData,
